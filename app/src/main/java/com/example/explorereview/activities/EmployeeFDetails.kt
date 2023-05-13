bce86b8ad299927933258c7568ee7653f15eac27
IT21227486 <it21227486@my.sliit.lk>
2023-05-13 20:47:25 +0530

    initial review2 commit messege

--- app/src/main/java/com/example/explorereview/activities/EmployeeFDetails.kt
+++ app/src/main/java/com/example/explorereview/activities/EmployeeFDetails.kt
@@ -1,10 +1,17 @@
 package com.example.explorereview.activities
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.explorereview.R
+import com.example.explorereview.models.FeedBackModel
+import com.google.firebase.database.FirebaseDatabase
 
 class EmployeeFDetails : AppCompatActivity() {
 
@@ -20,6 +27,36 @@ class EmployeeFDetails : AppCompatActivity() {
 
         initView()
         setValuesToViews()
+
+        btnFUpdate.setOnClickListener {
+            openUpdateReviewDialog(
+                intent.getStringExtra("feedbackId").toString(),
+                intent.getStringExtra("feedbackName").toString()
+            )
+        }
+
+        btnFDelete.setOnClickListener{
+            deleteFeedBackRecord(
+                intent.getStringExtra("feedbackId").toString()
+            )
+        }
+    }
+
+    private fun deleteFeedBackRecord(
+        fid: String
+    ) {
+        val dbRef = FirebaseDatabase.getInstance().getReference("Feedbacks").child(fid)
+        val fTask = dbRef.removeValue()
+
+        fTask.addOnSuccessListener {
+            Toast.makeText(this, "Feedback data deleted", Toast.LENGTH_LONG).show()
+
+            val intent = Intent(this, ReviewFetchingActivity::class.java)
+            finish()
+            startActivity(intent)
+        }.addOnFailureListener { error ->
+            Toast.makeText(this, "Deleting Err ${error.message}", Toast.LENGTH_LONG).show()
+        }
     }
 
     private fun initView() {
@@ -36,5 +73,57 @@ class EmployeeFDetails : AppCompatActivity() {
         tvFEmpName.text = intent.getStringExtra("feedbackName")
         tvFEmpMsg.text = intent.getStringExtra("feedbackMsg")
 
+    }
+
+    //@SuppressLint("MissingInflatedId")
+    private fun openUpdateReviewDialog(
+        feedbackId: String,
+        feedbackName: String
+    ){
+        val fDialog = AlertDialog.Builder(this)
+        val inflater = layoutInflater
+        val fDialogView = inflater.inflate(R.layout.activity_update_dialog, null)
+
+        fDialog.setView(fDialogView)
+
+        val etFEmpName = fDialogView.findViewById<EditText>(R.id.etFREmpName)
+        val etFEmpMsg = fDialogView.findViewById<EditText>(R.id.etFREmpMag)
+        val btnUpdateData = fDialogView.findViewById<Button>(R.id.btnFRUpdateData)
+
+        etFEmpName.setText(intent.getStringExtra("feedbackName").toString())
+        etFEmpMsg.setText(intent.getStringExtra("feedbackMsg").toString())
+
+        fDialog.setTitle("Updating $feedbackName Record")
+
+        val alertDialog = fDialog.create()
+        alertDialog.show()
+
+        btnUpdateData.setOnClickListener{
+            updateFeedbackData(
+                feedbackId,
+                etFEmpName.text.toString(),
+                etFEmpMsg.text.toString()
+            )
+
+            Toast.makeText(applicationContext, "Feedback Data Updated.", Toast.LENGTH_LONG).show()
+
+            // we are setting updated data to our textviews
+            tvFEmpName.text = etFEmpName.text.toString()
+            tvFEmpMsg.text = etFEmpMsg.text.toString()
+
+            alertDialog.dismiss()
+
+        }
+
+    }
+
+    private fun updateFeedbackData(
+        fid: String,
+        fname: String,
+        fmsg: String
+    ){
+        val dbRef = FirebaseDatabase.getInstance().getReference("Feedbacks").child(fid)
+        val empInfo = FeedBackModel(fid, fname, fmsg)
+        dbRef.setValue(empInfo)
     }
 }
---
+++ app/src/main/java/com/example/explorereview/activities/Update_Dialog.kt
@@ -0,0 +1,12 @@
+package com.example.explorereview.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.explorereview.R
+
+class Update_Dialog : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_dialog)
+    }
+}
